d82967b740b6a123f8cca4a14c8db33afe2edb67
pranav <pranavswarup.k@gmail.com>
2026-06-19 15:12:28 +0530

    Set GitHub Pages base/homepage for remodel-sa4s -> sa4s-redesign

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "https://sa4s-serc.github.io",
+  //base: "https://sa4s-serc.github.io",
+  base: "/sa4s-redesign/",
 }));
